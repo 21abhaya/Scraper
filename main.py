3d538acc670e55a77b008ce8b6c220fbdefc2d5a
abhaya <gautamabhaya98@gmail.com>
2025-11-21 23:28:39 +0545

    refactor: made changes in the code to generate separate json files for each category
    refactor: implemented logger to trace code behavior and errors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import logging 
 
 from pathlib import Path
 from dotenv import load_dotenv
@@ -8,8 +9,12 @@ from dotenv import load_dotenv
 
 
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='scraper_main.log', level=logging.INFO)
+
 BASE_DIR = Path(__file__).resolve().parent
-file_path = os.path.join(BASE_DIR, 'scraped_output.json')
+def set_file_name(name):
+    return os.path.join(BASE_DIR, f'{name}-tests.json' if name else 'tests.json')
 
 load_dotenv(BASE_DIR / '.env')
 
@@ -28,36 +33,59 @@ payload = {
     'password': password
 }
 
+index_mapping = {
+    1: "Biochemistry",
+    2: "Microbiology",
+    3: "Haematology",
+    6: "Serology",
+    7: "Histopathology",
+    8: "Immunoassay",
+    9: "ImmunoHistoChemistry",
+    10: "Molecular Biology",
+    11: "Immunology",
+    12: "Parasitology",
+    21: "Cytogenetics",
+    23: "Others",
+    26: "Package",
+    30: "Clinical Pathology",
+    31: "Molecular Pathology",
+    33: "Cytopthology"
+}
+
 try:
     login_response = session.post(login_url, data=payload)
     
     if login_response.status_code == 200:
-        print("Login successful!", login_response.url)
+        logger.info(f"Login successful!, This is the success URL: {login_response.url}")
         
-        for x in range (5):
-            print(protected_url)
-            data_response = session.get(f"{protected_url}testresult/get_tests/{x}")
-         
+        for key, value in enumerate(range(34), start=1):
+            if key not in index_mapping:
+                logger.info(f"{key} not in mapping, skipping...")
+                continue
+            data_response = session.get(f"{protected_url}testresult/get_tests/{key}")
+            logger.info(f"Fetching data from: {protected_url}testresult/get_tests/{key}")
+            label = index_mapping[key]
+            logger.info(f"Scraping data for category: {label}")
+
             if data_response.status_code == 200:
                 scraped_data = data_response.json()
-                print(scraped_data)
             
             try:
-                with open(file_path, 'w') as file:
+                with open(set_file_name(label), 'w') as file:
                     json.dump(
                         scraped_data,
                         file,
                         indent=4
                     )
-                    print(f"Text from scraped content successfully written into {file_path}")
-            except Exception as e:
-                print("Error writing to file:", e) 
+                    logger.info(f"Text from scraped content successfully written into {set_file_name(label)}")
+            except Exception:
+                logger.exception(f"Error writing to file {set_file_name(label)}:") 
         
     else:
-        print(f"Login failed. Status Code: {login_response.status_code}")
+        logger.error(f"Login failed. Status Code: {login_response.status_code}")
 
-except Exception as e:
-    print("An error occurred during scraping:", e)
+except Exception:
+    logger.exception("An error occurred during scraping:")
 
 
 
